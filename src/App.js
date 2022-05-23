ca285a7c6943a495aa88a9e10feb92260b530ba2
gevo222 <georgehovakimyan@outlook.com>
2022-05-23 01:05:50 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-purple-500">
+      <h1 className="text-xl">hello</h1>
+      <button className="btn">CLICK</button>
+    </div>
+  );
+}
+
+export default App;
