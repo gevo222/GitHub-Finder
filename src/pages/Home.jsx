8c870c1345e294870ee52ce28e923b642f13968d
George Hovakimyan <georgehovakimyan@outlook.com>
2022-05-26 17:32:00 -0700

    Started using Github API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 function Home() {
-  return <div className="text-6xl">Home</div>;
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  async function getUsers() {
+    const response = await fetch("https://api.github.com/users");
+    const data = await response.json();
+    setUsers(data);
+    setIsLoading(false);
+  }
+
+  if (!isLoading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    );
+  } else {
+    return <h3>Loading...</h3>;
+  }
 }
 
 export default Home;
