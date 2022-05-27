5841a671907fa1753089b87ee70f2bb0ec6cbe38
George Hovakimyan <georgehovakimyan@outlook.com>
2022-05-26 17:10:03 -0700

    Updated pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">GitHub Finder</h1>
+      <p className="mb-20 text-2xl font-light">
+        A React app to search GitHub profiles and see profile details.
+      </p>
+      <a href="https://gevo222.github.io/#/portfolio" className="text-3xl mb-4">
+        My Portfolio
+      </a>
+    </div>
+  );
 }
 
 export default About;
