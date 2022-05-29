edf196857b55511d3f2dbde15cf43fc4ff1fa09d
George Hovakimyan <georgehovakimyan@outlook.com>
2022-05-29 16:20:46 -0700

    Added spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,15 @@
+import spinner from "./layout/assets/spinner.gif";
+function Spinner() {
+  return (
+    <div className="w-100 mt-20">
+      <img
+        className="text-center mx-auto"
+        src={spinner}
+        width="180"
+        alt="Loading"
+      />
+    </div>
+  );
+}
+
+export default Spinner;
---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import Spinner from "../Spinner";
+
+function UserResults() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  async function getUsers() {
+    const response = await fetch("https://api.github.com/users");
+    const data = await response.json();
+    setUsers(data);
+    setIsLoading(false);
+  }
+
+  if (isLoading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner></Spinner>;
+  }
+}
+
+export default UserResults;
